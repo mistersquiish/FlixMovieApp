addb14a5e521b0b29494104dedad8a8963848e5b
Henry Vuong <henryvuong@henrys-macbook-air.local>
2018-02-11 16:33:24 -0600

    Initial Commit

---
+++ FlixPart1Assignment1Tests/FlixPart1Assignment1Tests.swift
@@ -0,0 +1,36 @@
+//
+//  FlixPart1Assignment1Tests.swift
+//  FlixPart1Assignment1Tests
+//
+//  Created by Henry Vuong on 2/11/18.
+//  Copyright © 2018 Henry Vuong. All rights reserved.
+//
+
+import XCTest
+@testable import FlixPart1Assignment1
+
+class FlixPart1Assignment1Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
